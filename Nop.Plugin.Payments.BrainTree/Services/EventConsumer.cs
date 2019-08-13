d9872a9ef679ea245beb8e080beac42fd99c4c35
Jigar <jiga.sangoi1990@gmail.com>
2019-08-13 17:23:42 +0530

    3D secure supported with new 3DS2

---
+++ Nop.Plugin.Payments.BrainTree/Services/EventConsumer.cs
@@ -0,0 +1,58 @@
+﻿using Nop.Services.Events;
+using Nop.Services.Payments;
+using Nop.Web.Framework.Events;
+using Nop.Web.Framework.UI;
+using System.Linq;
+
+namespace Nop.Plugin.Payments.BrainTree.Services
+{
+    /// <summary>
+    /// Represents event consumer of the Braintree payment plugin
+    /// </summary>
+    public class EventConsumer : IConsumer<PageRenderingEvent>
+    {
+        #region Fields
+
+        //private readonly ILocalizationService _localizationService;
+        private readonly IPaymentService _paymentService;
+        //private readonly IScheduleTaskService _scheduleTaskService;
+
+        #endregion
+
+        #region Ctor
+
+        public EventConsumer(//ILocalizationService localizationService,
+            IPaymentService paymentService)
+        {
+            //this._localizationService = localizationService;
+            this._paymentService = paymentService;
+            //this._scheduleTaskService = scheduleTaskService;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Handle page rendering event
+        /// </summary>
+        /// <param name="eventMessage">Event message</param>
+        public void HandleEvent(PageRenderingEvent eventMessage)
+        {
+            if (eventMessage?.Helper?.ViewContext?.ActionDescriptor == null)
+                return;
+
+            //check whether the plugin is installed and is active
+            var squarePaymentMethod = _paymentService.LoadPaymentMethodBySystemName("Payments.BrainTree");
+            if (!(squarePaymentMethod?.PluginDescriptor?.Installed ?? false) || !_paymentService.IsPaymentMethodActive(squarePaymentMethod))
+                return;
+
+            //add js script to one page checkout
+            if (eventMessage.GetRouteNames().Any(r => r.Equals("CheckoutOnePage")))
+            {
+                eventMessage.Helper.AddScriptParts(ResourceLocation.Footer, BraintreePaymentDefaults.BraintreeScriptPath, excludeFromBundle: true);
+                eventMessage.Helper.AddScriptParts(ResourceLocation.Footer, BraintreePaymentDefaults.BraintreeClientScriptPath, excludeFromBundle: true);
+                eventMessage.Helper.AddScriptParts(ResourceLocation.Footer, BraintreePaymentDefaults.BraintreeHostedFieldsScriptPath, excludeFromBundle: true);
+                eventMessage.Helper.AddScriptParts(ResourceLocation.Footer, BraintreePaymentDefaults.Braintree3DSecureScriptPath, excludeFromBundle: true);
+            }
+        }
+    }
+}
--- Nop.Plugin.Payments.BrainTree/Validators/PaymentInfoValidator.cs
+++ Nop.Plugin.Payments.BrainTree/Validators/PaymentInfoValidator.cs
@@ -16,10 +16,10 @@ namespace Nop.Plugin.Payments.BrainTree.Validators
             //RuleFor(x => x.CardNumber).NotEmpty().WithMessage(localizationService.GetResource("Payment.CardNumber.Required"));
             //RuleFor(x => x.CardCode).NotEmpty().WithMessage(localizationService.GetResource("Payment.CardCode.Required"));
 
-            RuleFor(x => x.CardholderName).NotEmpty().WithMessage(localizationService.GetResource("Payment.CardholderName.Required"));
-            RuleFor(x => x.CardNumber).IsCreditCard().WithMessage(localizationService.GetResource("Payment.CardNumber.Wrong"));
-            RuleFor(x => x.CardCode).Matches(@"^[0-9]{3,4}$").WithMessage(localizationService.GetResource("Payment.CardCode.Wrong"));
-            RuleFor(x => x.ExpireMonth).NotEmpty().WithMessage(localizationService.GetResource("Payment.ExpireMonth.Required"));
-            RuleFor(x => x.ExpireYear).NotEmpty().WithMessage(localizationService.GetResource("Payment.ExpireYear.Required"));
+            //RuleFor(x => x.CardholderName).NotEmpty().WithMessage(localizationService.GetResource("Payment.CardholderName.Required"));
+            //RuleFor(x => x.CardNumber).IsCreditCard().WithMessage(localizationService.GetResource("Payment.CardNumber.Wrong"));
+            //RuleFor(x => x.CardCode).Matches(@"^[0-9]{3,4}$").WithMessage(localizationService.GetResource("Payment.CardCode.Wrong"));
+            //RuleFor(x => x.ExpireMonth).NotEmpty().WithMessage(localizationService.GetResource("Payment.ExpireMonth.Required"));
+            //RuleFor(x => x.ExpireYear).NotEmpty().WithMessage(localizationService.GetResource("Payment.ExpireYear.Required"));
         }}
 }
