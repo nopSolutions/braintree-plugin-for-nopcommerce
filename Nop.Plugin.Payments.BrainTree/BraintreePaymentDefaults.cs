5e0c24afe9a6d23ea53d075d493bd2bc70d3892f
skoshelev <sergey.k@nopcommerce.com>
2020-06-08 15:59:03 +0300

    Update SDK version to 4.17

--- Nop.Plugin.Payments.BrainTree/BraintreePaymentDefaults.cs
+++ Nop.Plugin.Payments.BrainTree/BraintreePaymentDefaults.cs
@@ -6,10 +6,10 @@
 
         public static string BraintreeScriptPath => "https://js.braintreegateway.com/v2/braintree.js";
 
-        public static string BraintreeClientScriptPath => "https://js.braintreegateway.com/web/3.50.0/js/client.min.js";
+        public static string BraintreeClientScriptPath => "https://js.braintreegateway.com/web/3.62.1/js/client.min.js";
 
-        public static string BraintreeHostedFieldsScriptPath => "https://js.braintreegateway.com/web/3.50.0/js/hosted-fields.min.js";
+        public static string BraintreeHostedFieldsScriptPath => "https://js.braintreegateway.com/web/3.62.1/js/hosted-fields.min.js";
 
-        public static string Braintree3DSecureScriptPath => "https://js.braintreegateway.com/web/3.50.0/js/three-d-secure.min.js";
+        public static string Braintree3DSecureScriptPath => "https://js.braintreegateway.com/web/3.62.1/js/three-d-secure.min.js";
     }
 }
